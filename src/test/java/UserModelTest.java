d8eb5c69c868dde9db2aa3405e64541cd957ca3a
NotAHorse <66544358+laiweihong2020@users.noreply.github.com>
2022-08-28 22:28:30 +1000

    Added test cases for the login functions

--- src/test/java/UserModelTest.java
+++ src/test/java/UserModelTest.java
@@ -1,12 +1,15 @@
 import Entity.User;
 import Mapper.Mapper;
 import Model.UserModel;
+import Util.JWTUtil;
+import io.jsonwebtoken.JwtException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static Util.JWTUtil.validateToken;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserModelTest {
@@ -21,7 +24,7 @@ public class UserModelTest {
     @Test
     public void successfulRegistration() {
         // Set uMapper to return false (i.e. no duplicate names)
-        mapper.setCanInsert(true);
+        mapper.setEmptyResult(true);
         boolean isSuccessful = userModel.register(new User());
         assertTrue(isSuccessful);
     }
@@ -29,22 +32,44 @@ public class UserModelTest {
     @Test
     public void failedRegistration() {
         // Exhibits the behaviour when there are duplicate names
-        mapper.setCanInsert(false);
+        mapper.setEmptyResult(false);
         boolean isSuccessful = userModel.register(new User());
         assertFalse(isSuccessful);
     }
 
+    @Test
+    public void successfulLogin() {
+        mapper.setEmptyResult(false);
+        String jwt = userModel.login(new User());
+        assertNotNull(jwt);
+
+        // Make sure that the jwt token is valid
+        try {
+            boolean isValid = JWTUtil.validateToken(jwt);
+            assertTrue(isValid);
+        } catch(Exception ex) {
+
+        }
+    }
+
+    @Test
+    public void failedLogin() {
+        mapper.setEmptyResult(true);
+        String jwt = userModel.login(new User());
+        assertNull(jwt);
+    }
+
     class mockUserMapper extends Mapper<User> {
-        private boolean canInsert = false;
+        private boolean isEmptyResult = false;
         public List<User> result = new ArrayList<>();
 
         public mockUserMapper() {
 
         }
 
-        public void setCanInsert(boolean canInsert) {
-            this.canInsert = canInsert;
-            if(canInsert) {
+        public void setEmptyResult(boolean emptyResult) {
+            this.isEmptyResult = emptyResult;
+            if(emptyResult) {
                 result = new ArrayList<>();
             } else {
                 result.add(new User());
@@ -52,7 +77,7 @@ public class UserModelTest {
         }
 
         public boolean insert(User user) {
-            return this.canInsert;
+            return this.isEmptyResult;
         }
 
         public boolean delete(User u) {
