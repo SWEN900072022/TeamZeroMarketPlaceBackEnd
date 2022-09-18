b3349a0e7375b21ef675619cede7f46e7c0fb22c
NotAHorse <66544358+laiweihong2020@users.noreply.github.com>
2022-09-18 19:31:24 +1000

    Update CORS filter to accept jwt headers

--- src/main/java/Cors/CORSFilter.java
+++ src/main/java/Cors/CORSFilter.java
@@ -44,6 +44,7 @@ public class CORSFilter implements Filter {
         // Authorize (allow) all domains to consume the content
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "jwt");
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
